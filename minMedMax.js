d421fa88ffbcf1403eb40eb6668dc585cbff814a
Chonticha <Chonticha.it@mail.kmutt.ac.th>
2022-09-10 02:32:21 +0700

    finish

--- minMedMax.js
+++ minMedMax.js
@@ -2,5 +2,10 @@ const { template } = require('@babel/core')
 
 function minMedMax(n1, n2, n3) {
   //code here
+  let min=Math.min(n1, n2, n3)
+  let med=[n1,n2,n3]
+  med.sort(function (a, b) {return a - b})
+  let max=Math.max(n1,n2,n3)
+  return {min: min , mid: med[1], max: max}
 }
 module.exports = minMedMax
